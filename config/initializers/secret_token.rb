80fb1d95b3fb93afa57ef08703a899fe1ad88408
Johnnie Lo <Johnnielo50@gmail.com>
2014-06-28 23:33:22 -0700

    Created project Jaunt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jaunt::Application.config.secret_key_base = 'b9804f93f5450cb6db755af3b8729e48c1d1b522bedbe439bde9bdf0473144ba97ae10ad44214a35174910e8567a8ee8efdac1d7c064e8a9c7ca494c2f04dbfa'
